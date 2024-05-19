8bb82cb8da562cbf54bdecc8ddc04fd3e2bf36ca
Leotaxx <lyleride@gmail.com>
2024-05-19 23:06:53 +0100

    change text

--- app/recipes/koreanfood/page.tsx
+++ app/recipes/koreanfood/page.tsx
@@ -7,7 +7,7 @@ export default function Recipes() {
 
     return (
         <div className="flex flex-col justify-center items-center">
-            <h2>This is the Korean food starter recipes page</h2>
+            <h2>This is the Korean food recipes page</h2>
             <Link href="/recipes/koreanfood/starters" className="bg-green-400 text-white font-semibold m-4 py-3 px-6 rounded-lg shadow-md hover:bg-green-500 transition duration-300 ease-in-out">
                 Starter
             </Link>
